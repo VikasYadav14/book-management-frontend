0d1c977ee395115f469ec0d5797683ee6d0a1a8d
VikasYadav14 <110589727+VikasYadav14@users.noreply.github.com>
2023-01-09 15:36:25 +0530

    project start

--- pages/books/[bookId].js
+++ pages/books/[bookId].js
@@ -6,33 +6,31 @@ import BookImg from '../../public/book1.png';
 import useSWR from 'swr';
 
 export default function ViewBooks() {
-  const {bookId} = useRouter().query
-  console.log(bookId)
+  const { bookId } = useRouter().query;
+  console.log(bookId);
+
   const [bookData, setBookData] = useState([]);
 
-  useEffect(() => {
-    if(bookData){
-    fetch(`/book/${bookId}`)
+  useEffect((bookId) => {
+    if (!bookData) {
+      fetch(`book/${bookId}`)
       .then((response) => response.json())
       .then((output) => {
-        setBookData(output.data);
+      setBookData(output.data);
       });
-    }
-  });
- 
+    }});
 
-  
   // const bookData = data.data
-// const {_id, title, excerpt, userId, ISBN, category, subcategory, reviews, isDeleted, releasedAt, createdAt, updatedAt, reviewsData}=book
+  // const {_id, title, excerpt, userId, ISBN, category, subcategory, reviews, isDeleted, releasedAt, createdAt, updatedAt, reviewsData}=book
 
-console.log(bookData)
+  console.log(bookData);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100 ">
       <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
           <div className="flex justify-center w-2/5 bg-blue-500 rounded-tl-2xl rounded-bl-2xl">
-            <Image src={BookImg} className="m-5" alt='/' />
+            <Image src={BookImg} className="m-5" alt="/" />
           </div>
           <div className="text-gray-400 w-3/5 p-5">
             <div className="text-left font-bold text-gray-400">
--- pages/books/index.js
+++ pages/books/index.js
@@ -25,37 +25,36 @@ export default function Books() {
       <div className="px-20 py-20 bg-gray-100">
         <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-20 bg-white rounded-2xl">
           {/* <!--Card 1--> */}
-          {booksList.map((idx) => (
-            <Link
-              href={{
-                pathname: '/books/[bookId]',
-                query: { bookId: `${idx._id}` },
-              }}
-            >
-              <div className="overflow-hidden shadow-lg rounded-2xl border-2 border-blue-500">
-                <div className="w-full bg-gray-100">
-                  <Image className="w-full h-64" src={img} alt="/"></Image>
-                </div>
-                <div className="bg-blue-500 text-white ">
-                  <div className="px-6 py-4 ">
-                    <div className="font-bold text-xl m-2">{idx.title}</div>
-                    <p className="m-2">{idx.excerpt}</p>
+          {booksList.length > 0 &&
+            booksList.map((idx) => (
+              <div
+                key={idx._id}
+                className="overflow-hidden shadow-lg rounded-2xl border-2 border-blue-500"
+              >
+                <Link href={`/books/${idx._id}`}>
+                  <div className="w-full bg-gray-100">
+                    <Image className="w-full h-64" src={img} alt="/"></Image>
                   </div>
-                  <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
-                      Category : {idx.category}
-                    </span>
-                    <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
-                      Release : {idx.releasedAt}
-                    </span>
-                    <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
-                      Review : {idx.reviews}
-                    </span>
+                  <div className="bg-blue-500 text-white ">
+                    <div className="px-6 py-4 ">
+                      <div className="font-bold text-xl m-2">{idx.title}</div>
+                      <p className="m-2">{idx.excerpt}</p>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                      <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
+                        Category : {idx.category}
+                      </span>
+                      <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
+                        Release : {idx.releasedAt}
+                      </span>
+                      <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
+                        Review : {idx.reviews}
+                      </span>
+                    </div>
                   </div>
-                </div>
+                </Link>
               </div>
-            </Link>
-          ))}
+            ))}
         </div>
       </div>
     </section>
